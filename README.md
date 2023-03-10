394e8fceb9561243b25d9d0e664bf4a24d6d8482
pocker-lab <2548171533@qq.com>
2023-03-11 00:50:25 +0800

    20230311

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# PythonLearn
-python学习笔记
+# PyLearn
+
+## 参考链接
+
+1. [jobbole/awesome-python-cn | 包资源介绍](https://github.com/jobbole/awesome-python-cn)  
+2. [walter201230/Python | python学习](https://github.com/walter201230/Python)
+3. [wistbean/learn_python3_spider | python爬虫](https://github.com/wistbean/learn_python3_spider)
+
+## 更改pip代理链接
+
+```shell
+pip config set global.index-url http://pypi.douban.com/simple | pip config set global.trusted-host pypi.douban.com | pip config set global.timeout 6000
+```
+
+## sql
+
+[Learning/SQL](Learning/SQL.md)
+[MySql数据库的列类型（字段类型）](https://blog.csdn.net/xiaotom5/article/details/8140679)
+
+## python
+
+[python函数之传递多个参数](https://blog.csdn.net/u011607898/article/details/107585700)
+[在 Python 中合并列表的5种方法](https://cloud.tencent.com/developer/article/1815842)
---
+++ datacenter/sina.py
@@ -0,0 +1,102 @@
+import json
+
+import requests
+from pymysql import Connection
+
+
+def GetSina(page: int, num: int) -> str:
+    """
+    获取新浪基金数据
+    `page`: 页数， `num`：每页数据量
+    """
+    url = 'http://vip.stock.finance.sina.com.cn/fund_center/data/jsonp.php/IO.XSRV2.CallbackList[''%279o_rfPFvmkgcHnSk%27' \
+      ']/NetValueReturn_Service.NetValueReturnOpen?'
+
+    headers = {
+        'User-Agent': 'Apifox/1.0.0 (https://www.apifox.cn)',
+        'Accept': '*/*',
+        'Host': 'vip.stock.finance.sina.com.cn',
+        'Connection': 'keep-alive'
+    }
+
+    params = {
+        "page": page,
+        "num": num,
+        "sort": "zmjgm",
+        "asc": "0",
+        "ccode": "",
+        "type2": "",
+        "type3": ""
+    }
+
+    res = requests.request("GET", url, headers=headers, data={}, params=params)
+    return res.text
+
+
+def ListSun(*lists: list) -> list:
+    """
+    多个list列表合并
+    """
+    listdata = []
+    for item in lists:
+        listdata.extend(item)
+    return listdata
+
+
+def GetCursor():
+    """
+    实例化mysql数据库的对象
+    """
+    conn = Connection(
+        host='localhost',  # ip地址
+        port=3306,  # 端口，默认3306
+        user='root',  # 账号
+        password='123456'  # 密码
+    )
+
+    return conn
+
+
+# ----------->
+# 获取数据并转换为dict
+sinadict1 = json.loads(GetSina(1, 2)[91:-2])
+sinadict2 = json.loads(GetSina(2, 2)[91:-2])
+
+print(f"<-- sina基金总数：{sinadict1['total_num']} -->")
+
+print("-" * 100)
+# 合并基金数据
+datalist = ListSun(sinadict1['data'], sinadict1['data'])
+print(f"<-- 获取的基金总数{len(datalist)} -->")
+
+print("-" * 100)
+# 将基金数据写入到json文件中
+with open("D:/PyLearn/datacenter/sina.json", "w", encoding="utf8") as f:
+    f.write(json.dumps(datalist, ensure_ascii=False))
+    print("<-- 数据已写入 -->")
+    print("-" * 100)
+
+# 将数据写入到数据库中
+conn = GetCursor()
+cursor = conn.cursor()
+
+for line in datalist:
+    sqlcarry = f"insert into pydb.sina(symbol, name, clrq, jjjl) values ('{line['symbol']}', '{line['name']}', '{line['clrq']}', '{line['jjjl']}')"
+    try:
+        # 执行sql语句
+        cursor.execute(sqlcarry)
+        # 提交到数据库执行
+        conn.commit()
+    except Exception as e:
+        # 发生错误时回滚
+        print(f"-> 报错Error：{e}")
+        print(f"-> 执行语句：{repr(sqlcarry)}")
+        conn.rollback()
+        continue
+
+cursor.execute("select count(*) from pydb.sina")
+print("数据库中现有{}".format(cursor.fetchall()[0][0]))
+
+print("-" * 100)
+conn.close()  # 关闭到数据库的链接
+print("<-- 已关闭数据库链接 -->")
